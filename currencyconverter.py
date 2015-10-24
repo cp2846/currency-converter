8f50d744390b4c927078d1f863d494c87039e471
chemical_x <chemicidalmaniac@gmail.com>
2015-10-23 22:22:57 -0400

    updated algorithm to use regex terms

--- currencyconverter.py
+++ currencyconverter.py
@@ -6,14 +6,14 @@ import re
 
 #conversion script. Takes two arguments: mode and val
 #mode is the type of currency e.g. '£', '$' or '€'
-#val is the amount of currency, e.g. '400,000', '93.34', '0.39'
+#val is the amount of currency, e.g. '400000', '93.34', '0.39'
 def convert(mode, val):
-	
+
 	val = float(val)
 	#Make API call to fixer.io, load JSON data for exchange rates
 	url = urllib2.urlopen('http://api.fixer.io/latest?symbols=USD,GBP')
 	data = json.load(url)
-
+	print type(mode)
 	USD = data['rates']['USD']
 	GBP = data['rates']['GBP']
 	EUR = 1.00
@@ -23,80 +23,87 @@ def convert(mode, val):
 	if mode == "$":
 		converted_EUR = EUR / USD * val
 		converted_GBP = converted_EUR * GBP
-		converted_EUR = str(round(converted_EUR,2)) 
-		converted_GBP = str(round(converted_GBP,2))
-		return "$"+str(val)+" is £"+converted_GBP+", €"+converted_EUR
+		#format output with commas as thousands separators, round to two decimal places
+		converted_EUR = "{:,}".format(round(converted_EUR,2))
+		converted_GBP = "{:,}".format(round(converted_GBP,2))
+		val = "{:,}".format(val)
+		
+		return "$"+val+" => £"+converted_GBP+" or €"+converted_EUR
 		
 	elif mode == "£":
 		converted_EUR = EUR / GBP * val
 		converted_USD = converted_EUR * USD
-		converted_EUR = str(round(converted_EUR,2))
-		converted_USD = str(round(converted_USD,2))
-		return "£"+str(val)+"  is $"+converted_USD+", €"+converted_EUR
+		converted_EUR = "{:,}".format(round(converted_EUR,2))
+		converted_USD = "{:,}".format(round(converted_USD,2))
+		val = "{:,}".format(val)
+		return "£"+val+" => $"+converted_USD+" or €"+converted_EUR
 		
 	else:
 		converted_GBP = GBP * val 
 		converted_USD = val * USD
-		converted_USD = str(round(converted_USD,2))
-		converted_GBP = str(round(converted_GBP,2))
-		return "€"+str(val)+" is £"+converted_GBP+", $"+converted_USD
+		converted_USD = "{:,}".format(round(converted_USD,2))
+		converted_GBP = "{:,}".format(round(converted_GBP,2))
+		val = "{:,}".format(val)
+		return "€"+val+" => £"+converted_GBP+"or $"+converted_USD
 
 		
-#STRING PARSING ALGORITHM
-def parseString(string):
 
-	string = string.lower()
+def parseString(string):
+	
+	#hack to get some regexes to register correctly when term is at end of string
+	string += " "
+	string = string.encode("utf-8")
+	#REGEX PARAMETERS
+	type = r'([\$£€])'
+	number = r'([\d+.,]+)'
+	amounts = r'((million)?(m[\.\,\s])?(k[\.\,\s])?(thousand)?(billion)?(b[\.\,\s])?)'
+	matcher = re.compile(type+number+r"[\s]*"+amounts, re.UNICODE | re.IGNORECASE)
+	matches = re.findall(matcher, string)
 	
 	detected_currency = []
 	
-	#get currency type, USD, GBP, or EUR
-	type = r'([\$£€]{1})'
-	#Get digits, commas allowed
-	number = r'([\d+.,]+)'
-	#qualifiers such as "thousand", "million", "m", "k", etc
-	amounts = r'((million)?(m[\.\,\s])?(k[\.\,\s])?(thousand)?)'
-
-	matcher = re.compile(type+number+r"[\s]*"+amounts)
-	
-	matches = re.findall(matcher, string)
-	
-	
-	for i in matches:
-		type = i[0]
-		value = i[1]
+	for match in matches:
+		type = match[0]
+		value = match[1]
+		magnitude = match[2]
+		
 		if __checkValid(value):
-			value = __checkIfThousandsOrMillions(i[1],i[2])
+			value = __checkMagnitude(value,magnitude)
 			detected_currency.append([type,value])
 	
 	return detected_currency
-			
-def __checkValid(val):
-	if val.count(".") >= 2:
-		return False
-	if val == "":
-		return False
-	return True
+	
 
-def __checkIfThousandsOrMillions(val,string):
 	
-	string = string.replace(",","")
-	string = string.replace(" ","")
-	string = string.replace("."," ")
-	
-	val = str(val)
-	
-	val = val.replace(",", "")
-	
+def __checkValid(val):
+
 	if val.count(".") >= 2:
-		pass
+		return False
 		
+	return True
+	
+def __checkMagnitude(val,string):
+	string = __stripChars(string)
+	val = str(val)
+	val = val.replace(",", "")
 	val = float(val)
+	
+	if string == "billion" or string == "b":
+		val *= 1000000000
+	
 	if string == "million" or string == "m":
 		val *= 1000000
+		
 	elif string == "thousand" or string == "k":
 		val *= 1000
 	
 	return val
 
-
-
+#strips characters like " ", ".", etc. from "million,", "thousand " etc.
+def __stripChars(string):
+	string = string.replace(",","")
+	string = string.replace(" ","")
+	string = string.replace("."," ")
+	
+	return string
+	
